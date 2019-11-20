2b4c77ab89bf0dc8aa17f9229343e289bf040be5
Matthias Braun <matthias.braun@visus.uni-stuttgart.de>
2019-11-20 18:44:26 +0100

    comments

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -21,19 +21,17 @@ using namespace std;
 
 int main() {
 
-    std::string progName = "test";
-
     // PARAMETERS for Tracker and TrackingUtilizer /////////////////////////////
 
     /// Tracker Parameters
     tracking::Tracker::Params tp;
-    std::string active_node(""); /// ALL
+    std::string active_node(""); // Allowing all clients to access tracking data
     tp.active_node                   = active_node.c_str();
     tp.active_node_len               = active_node.length();
-    std::string client_ip("129.69.205.123"); /// (.76 = MINYOU) >>> Change to appropriate Client IP
+    std::string client_ip("129.69.205.123"); // TODO: Change to appropriate Client IP (.76 = MINYOU)
     tp.natnet_params.client_ip       = client_ip.c_str();
     tp.natnet_params.client_ip_len   = client_ip.length();
-    std::string server_ip("129.69.205.86"); /// (.86 = MINI)
+    std::string server_ip("129.69.205.86"); // (.86 = MINI)
     tp.natnet_params.server_ip       = server_ip.c_str();
     tp.natnet_params.server_ip_len   = server_ip.length();
     tp.natnet_params.cmd_port        = 1510;
@@ -57,11 +55,11 @@ int main() {
 
     /// TrackingUtilizer Parameters
     tracking::TrackingUtilizer::Params tup; 
-    device_name                      = bp.device_name; // Insert bp.device_name only for appropriate rigid body. 
+    device_name                      = bp.device_name; // TODO: Insert bp.device_name only for appropriate rigid body. 
     tup.btn_device_name              = device_name.c_str(); 
     tup.btn_device_name_len          = device_name.length();
     std::string rigid_body_name("");
-    tup.rigid_body_name              = rigid_body_name.c_str(); // Will be set in line 87 depending on what rigid bodies are available.
+    tup.rigid_body_name              = rigid_body_name.c_str(); // (Will be set in line 108 depending on what rigid bodies are available.)
     tup.rigid_body_name_len          = rigid_body_name.length();
     tup.select_btn                   = -1;
     tup.rotate_btn                   = -1;
@@ -82,9 +80,9 @@ int main() {
 
     // TRACKING INITIALISATION ////////////////////////////////////////////////
     
-    // Tracker
-    /// Handles all communication with NatNet and VRPN.
-    /// Creating one (!) Tracker which runs in separate thread.
+    // --- Tracker ---
+    // Handles all communication with NatNet and VRPN.
+    // Creating one (!) Tracker which runs in separate thread.
     auto tracker = std::make_shared<tracking::Tracker>();
     if (!tracker->Initialise(tp)) {
         std::cerr << std::endl << "[ERROR] [test] Unable to initialise <Tracker>. " <<  "[" << __FILE__ << ", " << __FUNCTION__ << ", line " << __LINE__ << "]" << std::endl << std::endl;
@@ -95,9 +93,10 @@ int main() {
         return 1;
     }
 
-    // TrackingUtilizers
-    /// Manage tracking data for one rigid body each.
-    /// Creating a TrackingUtilizer for each found rigid body (and corresponding button device).
+    // --- TrackingUtilizers ---
+    // Manage tracking data for one rigid body each.
+    // Creating a TrackingUtilizer for each found rigid body (and corresponding button device).
+    // Initialising TrackingUtilizer with exisiting Tracker.
     size_t rigiBodyCount = tracker->GetRigidBodyCount();
     std::vector<std::string> rigidBodies;
     for (size_t i = 0; i < rigiBodyCount; i++) {
@@ -125,13 +124,13 @@ int main() {
     bool exit = false;
     while (!exit) {
 
-        // Get current tracking data for each rigid body managed by a TrackingUtilizer.
+        // Get current tracking data for each rigid body managed by the TrackingUtilizers.
         for (auto& tu : utilizers) {
 
             // Field of View
             //state = tu.GetFieldOfView(fov);
             //std::cout << std::fixed << std::setprecision(4) <<
-            //    "[info] <" << progName.c_str() << "> RIGID-BODY \"" << tu.GetRigidBodyName().c_str() << "\" - FIELD OF VIEW (valid = "
+            //    "[INFO] [test] RIGID-BODY \"" << tu.GetRigidBodyName().c_str() << "\" - FIELD OF VIEW (valid = "
             //    << ((state) ? ("TRUE") : ("FALSE")) << ") ";
             //if (state) {
             //    std::cout << " - Coordinates: LEFT_TOP (" <<
@@ -145,15 +144,15 @@ int main() {
             // Intersection
             state = tu.GetIntersection(ist);
             std::cout << std::fixed << std::setprecision(4) <<
-                "[INFO] [" << progName.c_str() << "] RIGID-BODY \"" << tu.GetRigidBodyName() << "\" - INTERSECTION (valid = "
+                "[INFO] [test] RIGID-BODY \"" << tu.GetRigidBodyName() << "\" - INTERSECTION (valid = "
                 << ((state) ? ("TRUE") : ("FALSE")) << ") ";
             if (state) {
                 std::cout << " - Coordinates: (" << ist.X() << "," << ist.Y() << ") ";
             }
             std::cout << std::endl;
+
         }
         std::cout << std::endl;
-
 
         // Wait for user input to end loop
         std::cout << "[Press 'ESC' to exit program]" << std::endl << std::endl;
